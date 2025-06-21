cf6523200cd1bb21ee008c32d6167c3674939f0c
Rotcivosk <46512650+rotcivosk@users.noreply.github.com>
2025-06-20 21:27:30 -0400

    corrigir db

--- database.py
+++ database.py
@@ -8,6 +8,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL não configurado nas variáveis de ambiente do Railway.")
 
+
+# Corrigir postgres:// para postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
